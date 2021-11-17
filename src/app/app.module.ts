ac268d8e0e32f6f6dd50cabc11532e34591772b5
aliniyazi <aliniyazi@abv.bg>
2021-11-17 18:32:11 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
+const appRoutes: Routes = [
+  {path:'',component:MainComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    UsersComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+  public Username: string;
+  public Email: string;
+  public Password: string;
+  constructor(username: string, email: string, password: string) {
+    this.Username = username;
+    this.Email = email;
+    this.Password = password;
+  }
+}
